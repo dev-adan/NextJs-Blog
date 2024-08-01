174bb8cbacced48a3f3d38a64b8b74469695ac03
Adan <adanaligakhar@gmail.com>
2024-08-01 12:36:45 +0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "picsum.photos",
+        },
+        {
+          protocol: "https",
+          hostname: "healthwire.pk",
+        },
+        {
+          protocol: "https",
+          hostname: "images.pexels.com",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
